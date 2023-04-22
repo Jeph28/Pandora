2a99f73e53deee8ef57c54f35181cad9c3caaae3
Jose Perez <jperez@modu.one>
2023-04-22 15:59:49 -0400

    Agregue el funcionamiento de el checkpoint

---
+++ ProximityActiviteCheckPoint.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class ProximityActiviteCheckPoint : MonoBehaviour
+{
+    [SerializeField] public Transform distanceActivator;
+    private Transform lookAtActivator;
+    public float distance;
+    public Transform activator;
+    private bool activeState;
+    public CanvasGroup target;
+    public bool lookAtCamera;
+    // public bool enableInfoPanel = false;
+    [SerializeField] private TMP_Text Changetext;
+    //public GameObject infoIcon;
+    [SerializeField] private GameObject Result;
+    float alpha;
+    //public CanvasGroup infoPanel;
+    Quaternion originRotation, targetRotation;
+
+    void Start()
+    {
+        originRotation = transform.rotation;
+        alpha = activeState ? 1 : -1;
+        if (activator == null) activator = Camera.main.transform;
+        //infoIcon.SetActive(infoPanel != null);
+    }
+
+    bool IsTargetNear()
+    {
+        var distanceDelta = distanceActivator.position - activator.position;
+        if (distanceDelta.sqrMagnitude < distance * distance)
+        {
+            if (lookAtActivator != null)
+            {
+                var lookAtActivatorDelta = lookAtActivator.position - activator.position;
+                if (Vector3.Dot(activator.forward, lookAtActivatorDelta.normalized) > 0.95f)
+                    return true;
+            }
+            var lookAtDelta = target.transform.position - activator.position;
+            if (Vector3.Dot(activator.forward, lookAtDelta.normalized) > 0.95f)
+                return true;
+        }
+        return false;
+    }
+
+    void Update()
+    {
+        if (!activeState)
+        {
+            Debug.Log("Entro");
+            if (IsTargetNear())
+            {
+                alpha = 1;
+                activeState = true;
+                // enableInfoPanel = true;
+                Debug.Log("Funciona");
+            }
+        }
+        else
+        {
+            if (!IsTargetNear())
+            {
+                alpha = 1;
+                activeState = false;
+                Debug.Log("No Funciona");
+                // enableInfoPanel = false;
+                // Result.SetActive(false);
+            }
+        }
+
+        target.alpha = Mathf.Clamp01(target.alpha + alpha * Time.deltaTime);
+        
+        if (lookAtCamera)
+        {
+            if (activeState)
+                targetRotation = Quaternion.LookRotation(activator.position - transform.position);
+            else
+                targetRotation = originRotation;
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+        }
+        //if (infoPanel != null)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Y))
+        //    {
+        //        enableInfoPanel = !enableInfoPanel;
+        //        infoPanel.alpha = Mathf.Lerp(infoPanel.alpha, Mathf.Clamp01(enableInfoPanel ? alpha : 0), Time.deltaTime * 10);
+        //    }
+        //}
+    }
+
+    // public void DestroyScript(InputAction.CallbackContext callbackContext)
+    // {
+    //     if (activeState && callbackContext.performed)
+    //     {
+    //         Changetext.text = "Aspecto fisico : " + GameManager.PastaState + "\n" + "Pasta Producida : " + GameManager.PastaScore.ToString() + "\n";
+    //         Result.SetActive(true);
+    //     }
+        
+    // }
+}
