26768184edb5f5d2c44a0dae2bb2da1ab89a58a3
Jose Perez <jperez@modu.one>
2023-06-06 23:28:49 -0400

    Se fixio el switch 1

--- Switch1.cs
+++ Switch1.cs
@@ -33,7 +33,6 @@ public class Switch1 : MonoBehaviour
         originRotation = transform.rotation;
         alpha = activeState ? 1 : -1;
         if (activator == null) activator = Camera.main.transform;
-        animator = Worker.GetComponent<Animator>();
     }
 
     bool IsTargetNear()
